54dcc3c5007d83ffbea4fa81a2ac9992962c2b7a
mnsooralee <mnsooralee@gmail.com>
2025-02-08 13:44:05 +0500

    Update ProjectCardData.js

--- src/components/ProjectCardData.js
+++ src/components/ProjectCardData.js
@@ -23,8 +23,8 @@ const ProjectCardData = [
         imgsrc: project3,
         title: "Rock, Paper, Scissors Game",
         text:"Developed a Rock, Paper, Scissors game in HTML, CSS, JavaScript with real-time score tracking and dynamic outcomes using DOM manipulation, event listeners, and random choice generation. Applied JavaScript logic for game flow and user-PC interaction.",
-        view: "",
-        source: ""
+        view: "https://rockpaperscissorsapp.vercel.app/",
+        source: "https://rockpaperscissorsapp.vercel.app/"
     },
     {
         imgsrc: project4,
@@ -37,15 +37,15 @@ const ProjectCardData = [
         imgsrc: project5,
         title: "Portfolio website in React JS.",
         text:"Built a React JS website with dynamic content, responsive design, and a user-friendly navigation system using React Router. including cards showcasing data passed through props. Implemented a responsive design for seamless viewing across various devices.",
-        view: "",
-        source: ""
+        view: "https://mansoorahmed.vercel.app/",
+        source: "https://mansoorahmed.vercel.app/"
     },
     {
         imgsrc: project2,
         title: "Dynamic UI Login/Signup Page",
         text:"Developed a responsive login/signup page. The interface dynamically adjusts, hiding or disabling unnecessary fields based on user actions. Applied modern CSS styling for user friendly design with seamless transitions. Also featuring an auto-generate password option combining letters, numbers, and special characters for sign-up.",
-        view: "",
-        source: ""
+        view: "loginandsignupform.vercel.app",
+        source: "loginandsignupform.vercel.app"
     }
 ]
 export default ProjectCardData;
