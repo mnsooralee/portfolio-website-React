99c356b1c9ca30f86e7fc0301334af1fa51efd72
mnsooralee <mnsooralee@gmail.com>
2025-02-08 12:43:11 +0500

    Update ProjectCardData.js

--- src/components/ProjectCardData.js
+++ src/components/ProjectCardData.js
@@ -9,43 +9,43 @@ const ProjectCardData = [
         imgsrc: project1,
         title: "Responsive Homepage",
         text:"Developed a responsive one-page website using HTML and CSS. Implemented a dynamic navigation menu and a smooth side-bar toggle. Utilized CSS for styling and visual appeal, with a focus on responsiveness across different devices.",
-        view: "https://github.com/mnsooralee/digital-world-with-vj",
-        source: "https://github.com/mnsooralee/digital-world-with-vj"
+        view: "",
+        source: ""
     },
     {
         imgsrc: project2,
         title: "Dynamic UI Login/Signup Page",
         text:"Developed a responsive login/signup page. The interface dynamically adjusts, hiding or disabling unnecessary fields based on user actions. Applied modern CSS styling for user friendly design with seamless transitions. Also featuring an auto-generate password option combining letters, numbers, and special characters for sign-up.",
-        view: "https://github.com/mnsooralee/login-signup-form",
-        source: "https://github.com/mnsooralee/login-signup-form"
+        view: "",
+        source: ""
     },
     {
         imgsrc: project3,
         title: "Rock, Paper, Scissors Game",
         text:"Developed a Rock, Paper, Scissors game in HTML, CSS, JavaScript with real-time score tracking and dynamic outcomes using DOM manipulation, event listeners, and random choice generation. Applied JavaScript logic for game flow and user-PC interaction.",
-        view: "https://github.com/mnsooralee/rock-paper-scissors",
-        source: "https://github.com/mnsooralee/rock-paper-scissors"
+        view: "",
+        source: ""
     },
     {
         imgsrc: project4,
         title: "Weather App",
         text:"Developed a weather application in HTML, CSS, and JavaScript fetching real-time weather data from the OpenWeatherMap API. Displays weather details like temperature, humidity, wind speed, and relevant icons based on conditions.",
-        view: "https://github.com/mnsooralee/weather-app",
-        source: "https://github.com/mnsooralee/weather-app"
+        view: "",
+        source: ""
     },
     {
         imgsrc: project5,
         title: "Portfolio website in React JS.",
         text:"Built a React JS website with dynamic content, responsive design, and a user-friendly navigation system using React Router. including cards showcasing data passed through props. Implemented a responsive design for seamless viewing across various devices.",
         view: "https://mansoorahmed.vercel.app/",
-        source: "https://github.com/mnsooralee/portfolio-website-React"
+        source: "https://mansoorahmed.vercel.app/"
     },
     {
         imgsrc: project2,
         title: "Dynamic UI Login/Signup Page",
         text:"Developed a responsive login/signup page. The interface dynamically adjusts, hiding or disabling unnecessary fields based on user actions. Applied modern CSS styling for user friendly design with seamless transitions. Also featuring an auto-generate password option combining letters, numbers, and special characters for sign-up.",
-        view: "https://github.com/mnsooralee/login-signup-form",
-        source: "https://github.com/mnsooralee/login-signup-form"
+        view: "",
+        source: ""
     }
 ]
 export default ProjectCardData;
